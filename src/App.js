f1de665db27550e9b3c84dc7568db95bdbe48cc2
Elijah Manor <elijah.manor@gmail.com>
2020-05-09 11:55:53 -0500

    Before

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
-import React from "react";
-import "./styles.css";
+import React from 'react'
+import random from 'lodash.random'
+import logo, {
+  ReactComponent as ReactLogo
+} from './logo.svg'
+import {
+  transform,
+  motion,
+  AnimateSharedLayout
+} from 'framer-motion'
+import './App.css'
 
-export default function App() {
+const generateVotes = prevVotes => {
+  prevVotes[0].percentage = random(20, 80)
+  prevVotes[1].percentage = 100 - prevVotes[0].percentage
+  return [...prevVotes]
+}
+
+function App() {
+  const [votes, setVotes] = React.useState(
+    generateVotes([
+      {
+        party: 'R',
+        minColor: '#b8383d',
+        maxColor: '#f55385',
+        percentage: 50
+      },
+      {
+        party: 'D',
+        minColor: '#498ba7',
+        maxColor: '#3794ff',
+        percentage: 50
+      }
+    ])
+  )
+
+  React.useEffect(() => {
+    window.setTimeout(() => {
+      setVotes(generateVotes(votes))
+    }, 5000)
+  }, [votes])
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ReactLogo
+        src={logo}
+        className="App-logo"
+        alt="logo"
+      />
+      <AnimateSharedLayout>
+        <motion.ul className="Votes">
+          {votes.map(vote => (
+            <motion.li
+              animate
+              key={vote.party}
+              style={{
+                width: `${vote.percentage}%`,
+                background: transform(
+                  vote.percentage,
+                  [0, 100],
+                  [vote.minColor, vote.maxColor]
+                )
+              }}
+            >
+              <motion.span animate>
+                {vote.percentage}%
+              </motion.span>
+            </motion.li>
+          ))}
+        </motion.ul>
+      </AnimateSharedLayout>
     </div>
-  );
+  )
 }
+
+export default App
