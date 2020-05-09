301693c2d45e351ba65402c89ef6e5ea499d4ad9
Elijah Manor <elijah.manor@gmail.com>
2020-05-09 12:15:07 -0500

    Added prefers-reduced-motion code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import random from 'lodash.random'
+import React from 'react';
+import random from 'lodash.random';
 import logo, {
   ReactComponent as ReactLogo
-} from './logo.svg'
+} from './logo.svg';
 import {
   transform,
   motion,
-  AnimateSharedLayout
-} from 'framer-motion'
-import './App.css'
+  AnimateSharedLayout,
+  useReducedMotion
+} from 'framer-motion';
+import './App.css';
 
 const generateVotes = prevVotes => {
-  prevVotes[0].percentage = random(20, 80)
-  prevVotes[1].percentage = 100 - prevVotes[0].percentage
-  return [...prevVotes]
-}
+  prevVotes[0].percentage = random(20, 80);
+  prevVotes[1].percentage =
+    100 - prevVotes[0].percentage;
+  return [...prevVotes];
+};
 
 function App() {
+  const shouldReduceMotion = useReducedMotion();
+  // const [
+  //   shouldReduceMotion,
+  //   setShouldReduceMotion
+  // ] = React.useState(false);
+  // React.useEffect(() => {
+  //   const mediaQuery = window.matchMedia(
+  //     '(prefers-reduced-motion: reduce)'
+  //   );
+  //   const handleMedia = e =>
+  //     setShouldReduceMotion(e.matches);
+  //   mediaQuery.addEventListener('change', handleMedia);
+  //   return () =>
+  //     mediaQuery.removeEventListener(
+  //       'change',
+  //       handleMedia
+  //     );
+  // }, []);
   const [votes, setVotes] = React.useState(
     generateVotes([
       {
@@ -32,13 +52,13 @@ function App() {
         percentage: 50
       }
     ])
-  )
+  );
 
   React.useEffect(() => {
     window.setTimeout(() => {
-      setVotes(generateVotes(votes))
-    }, 5000)
-  }, [votes])
+      setVotes(generateVotes(votes));
+    }, 5000);
+  }, [votes]);
 
   return (
     <div className="App">
@@ -51,7 +71,9 @@ function App() {
         <motion.ul className="Votes">
           {votes.map(vote => (
             <motion.li
-              animate
+              animate={
+                shouldReduceMotion ? undefined : true
+              }
               key={vote.party}
               style={{
                 width: `${vote.percentage}%`,
@@ -62,7 +84,11 @@ function App() {
                 )
               }}
             >
-              <motion.span animate>
+              <motion.span
+                animate={
+                  shouldReduceMotion ? undefined : true
+                }
+              >
                 {vote.percentage}%
               </motion.span>
             </motion.li>
@@ -70,7 +96,7 @@ function App() {
         </motion.ul>
       </AnimateSharedLayout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
